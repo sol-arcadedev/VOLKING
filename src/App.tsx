1bb9fcc7e3a92711d9660142766ad8ddc4c3c852
sol.arcadedev <sol.arcadedev@gmail.com>
2026-01-03 14:22:44 +0100

    added live volumetracker for the contributed volume of wallets.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Leaderboard } from './components/Leaderboard';
+import { VolumeTracker } from './components/VolumeTracker';
 import { HallOfDegens } from './components/HallOfDegens';
 import { GlobalStats } from './components/GlobalStats';
 import { Rewards } from './components/Rewards';
@@ -18,6 +19,7 @@ const App: React.FC = () => {
                 <About />
                 <GlobalStats />
                 <Leaderboard />
+                <VolumeTracker />
                 <HallOfDegens />
                 <Rewards />
             </main>
